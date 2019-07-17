60d24dbd5511cde6ea136cacf09ed4d9421850cb
Artem <inste@icloud.com>
2019-07-17 19:19:34 +0300

    Initial Commit

---
+++ Threads3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Threads3
+//
+//  Created by Артём Кармазь on 7/17/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
